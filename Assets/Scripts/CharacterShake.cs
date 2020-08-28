961f6f1a98a73daf936682830d98343b6aa0cdd6
Abigail <asyelsub@gmail.com>
2020-08-28 15:36:31 -0400

    The most silly script is part of this game now... the wet dog shaking script. :P

---
+++ Assets/Scripts/CharacterShake.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Put this script on an object to have it shake frantically and randomly, like a cold dog that's come in from the rain.
+public class CharacterShake : MonoBehaviour
+{
+    private GameObject shakingObject;
+    private Transform shakingObjectTransform;
+    private Vector3 randomMovement;
+    private Vector3 initialPos;
+    public float shakeIntensity;
+
+    private void Start()
+    {
+        shakingObject = gameObject;
+        shakingObjectTransform = shakingObject.transform;
+        initialPos = shakingObject.transform.position;
+    }
+
+    private void Update()
+    {
+        //I've never had to use UnityEngine before Random.Range but for some reason I got an error. Whatever, I guess?
+        randomMovement = new Vector3(UnityEngine.Random.Range(-shakeIntensity,shakeIntensity),UnityEngine.Random.Range(-shakeIntensity,shakeIntensity), 0f);
+        shakingObjectTransform.position = initialPos + randomMovement;
+    }
+}
